15ef0e2852bc40f706a341acdafade13bbb6f81d
Summershinne <145506570+Summershinne@users.noreply.github.com>
2024-06-30 17:51:14 +0300

    hw

---
+++ src/server.js
@@ -0,0 +1,75 @@
+import express from "express";
+import pino from "pino-http";
+import cors from "cors";
+import env from "./utils/env.js"
+import { getContactById , getAllContacts} from "./services/contacts.js";
+
+const PORT = Number(env("PORT", "3000"));
+
+const setupServer = () => {
+    const app = express();
+    const logger = pino({
+        transport: {
+            target: "pino-pretty"
+        }
+    });
+
+    app.use(logger);
+    app.use(cors());// дозволяє обмінюватися інформацією між веб-ресурсами з різних доменів.
+
+    app.get("/", (req, res) => {
+        res.json({
+            status: 200,
+            message: 'Hello world!'
+        });
+    });
+
+    app.get('/contacts', async (req, res) => {
+        const contacts = await getAllContacts();
+        
+        res.status(200).json({
+            status: 200,
+            data: contacts,
+            message: "Successfully found contacts!"
+        })
+    });
+
+    app.get('/contacts/:contactId', async (req, res) => {
+        const { contactId } = req.params;
+        const contact = await getContactById(contactId);
+
+
+        res.status(200).json({
+            status: 200,
+            data: contact,
+            message: `Successfully found contact with id ${contactId}!`,
+        });
+
+        if (!contact) {
+            res.status(404).json({
+                status: 404,
+                message: 'Student not found'
+            });
+            return;
+        };
+
+    });
+
+    app.use('*', (req, res) => {
+        res.status(404).json({
+            status:404,
+            message: "Not found"
+        })
+    });
+
+    app.use((e, req, res) => {
+        res.status(500).json({
+            status: 500,
+            message: 'Something went wrong',
+            error: e.message
+        })
+    });
+
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+};
+export default setupServer;
