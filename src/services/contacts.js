622c7af42365c2f3d70fa30007d84beb0b575a63
Summershinne <145506570+Summershinne@users.noreply.github.com>
2024-06-30 18:05:16 +0300

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import Contact from "../db/models/contact.js";
+import Contact from "../db/models/Contact.js";
 
 export const getAllContacts = async () => {
     const contacts = await Contact.find();
